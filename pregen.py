f2188de36d0fd83a48690f8428404a9f44f1540a
uno/hurtki <asboba2101@gmail.com>
2025-09-14 11:26:56 +0300

    add ( docker-compose, env variables, error handling ) + add ( .gitignore )

--- pregen.py
+++ pregen.py
@@ -1,23 +1,29 @@
 import time
 from mcrcon import MCRcon
-PORT = 25575
-PASSWORD = ''
-PREGEN_RADIUS = 1000
+import os 
+
+
+PORT = int(os.getenv("MINECRAFT_SERVER_PORT"))
+PASSWORD = os.getenv("1234")
+PREGEN_RADIUS = int(os.getenv("PREGEN_RADIUS"))
+SERVER_ADRESS = os.getenv("SERVER_ADRESS")
+PING_DELAY=int(os.getenv("DELAY_BEETWEEN_PINGS"))
+
 
 def main():
-    with MCRcon("127.0.0.1",password=PASSWORD ,port=PORT) as con:
+    with MCRcon(SERVER_ADRESS,password=PASSWORD ,port=PORT) as con:
         enabled = False
         while True:
-            time.sleep(4)
+            time.sleep(PING_DELAY)
             try:
-                resp = con.connect("list");
+                resp = con.connect("list")
                 resp = resp[11:]
                 players = 0
                 for i in resp:
                     if i.isdigit():
-                        players == players*10 + int(i);
+                        players == players*10 + int(i)
                     if i == "/":
-                        break;
+                        break
                 if players <= 4:
                     if not enabled:
                         con.command(f"pregen start {PREGEN_RADIUS}")
@@ -31,4 +37,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except ConnectionRefusedError:
+        print(f"Connection refused, while trying to connect:{SERVER_ADRESS} on port:{PORT}")
+    except Exception as e:
+        print(e)
